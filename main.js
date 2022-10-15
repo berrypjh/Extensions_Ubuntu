1a68c21f6be580ce8b743472e57e607a65a97147
Junhyuk Park <junhyeg08@gmail.com>
2022-10-15 22:44:58 +0900

    Chore: 약간의 수정

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ const inputs = {
   systemFee: 0,
   networkFee: 0,
   // networkMagic: 1234567890, //CONST.MAGIC_NUMBER.TestNet,
-  networkMagic: CONST.MAGIC_NUMBER.TestNet,
+  // networkMagic: CONST.MAGIC_NUMBER.TestNet,
+  networkMagic: 1234567890, //CONST.MAGIC_NUMBER.TestNet,
   //   nodeUrl: "http://localhost:20332", //"http://seed2t.neo.org:20332",
   //   nodeUrl: "http://seed2t.neo.org:20332",
-  nodeUrl: "http://seed3t5.neo.org:20332",
+  // nodeUrl: "http://seed3t5.neo.org:20332",
+  nodeUrl: "http://localhost:50012",
 };
 
 const vars = {};
